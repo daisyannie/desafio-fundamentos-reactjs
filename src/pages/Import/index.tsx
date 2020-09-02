001d2924c5520204a656dfd0c70b45136cfdd600
Daisy Annie <daisy.annie@shift.com.br>
2020-09-02 06:40:38 -0300

    Desafio 7 - fundamentos de React.

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.forEach(uploadedFile => {
+      data.append('file', uploadedFile.file);
+    });
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      setUploadedFiles([]);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    files.forEach((file: File) => {
+      const fileProp: FileProps = {
+        file,
+        name: file.name,
+        readableSize: file.size.toString(),
+      };
+      setUploadedFiles([...uploadedFiles, fileProp]);
+    });
   }
 
   return (
